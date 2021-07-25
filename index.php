6e6653774385e1d16a6f510569378ebeddb66b1b
Eiborth <gomezeiborth@gmail.com>
2021-07-24 22:40:43 -0400

    Creacion de navbar con logo y opciones (aun faltan por definir)
    Se realizo una parte de la navbar alineando las opciones junto al logo cuestion de que quedaran al mismo nivel.
    
    - Se cargo el logoy se creo la carpeta img, para almacenar las imagenes.
    - Se creo el archivo main.css para enlazar y crear los estilos del portafolio.
    - Se definio previo codigo de la navbar  con su logo, enlaces a otros archivos y elementos.

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <link rel="stylesheet" href="css/main.css">
+	<title>Document</title>
+</head>
+
+<!-- Star Body -->
+<body>
+
+  <nav class="navbar">
+      <a class="navbar-logo" href="#">
+        <img class ="logo" src="img/zherodev_logo.png" alt="">
+      </a>
+      
+      <ul class="nav justify-content-end">
+        <li class="nav-item">
+          <a class="nav-link active" href="#">Active</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">Link</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+  </nav>   
+
+</body>
+<!-- End Body -->
+</html>
