36292ed647d2dcc91b3f9dd508b7ff038f909790
Eiborth <gomezeiborth@gmail.com>
2021-07-24 23:31:25 -0400

    Creacion de collapse menu
    Se desarrollo el menu colapsado para que cuando la vista de la web sea responsiva o se vea desde algun dispositivo, el menu se muestre en un toggler y sea mas dinamico para el usuario.
    
    - En el head se enlazo con los CDN de boostrap respectivos.
    - en la parte de la nav se establecio el codigo para la vista movil o responsive.
    - En el css se establecio el estilo que tendra el toggler del menu.

--- index.php
+++ index.php
@@ -4,20 +4,35 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="stylesheet" href="css/main.css">
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+  
 	<title>Document</title>
 </head>
 
 <!-- Star Body -->
 <body>
 
-  <nav class="navbar">
-      <a class="navbar-logo" href="#">
-        <img class ="logo" src="img/zherodev_logo.png" alt="">
-      </a>
-      
-      <ul class="nav justify-content-end">
+  <nav class="navbar navbar-expand-lg">
+    <!-- Logo -->
+    <a class="navbar-logo" href="#">
+      <img class ="logo" src="img/zherodev_logo.png" alt="">
+    </a>
+    <!-- boton toggle cuando la vista es en movil -->
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <!-- lineas del boton -->
+      <span class="line"></span> 
+      <span class="line"></span> 
+      <span class="line" style="margin-bottom: 0;"></span>
+
+    </button>
+    <!-- Opciones de menu (contenedo) -->
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+      <!-- Opciones de menu lista -->
+      <ul class="navbar-nav ">
         <li class="nav-item">
           <a class="nav-link active" href="#">Active</a>
         </li>
@@ -31,8 +46,10 @@
           <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
         </li>
       </ul>
+    </div>
   </nav>   
 
 </body>
 <!-- End Body -->
+
 </html>
