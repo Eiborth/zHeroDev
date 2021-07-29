7a8fb3d1a73c37b0e0fd1f1e15cb7a440f844e72
Eiborth <gomezeiborth@gmail.com>
2021-07-28 21:22:23 -0400

    Pie de pagina y animacion a index.php version pc
    Se agrego el pie de pagina junto algunos iconos de redes sociales al pie de pagina tambien en el css se colocaron los atributos y estilos correspondientes,
    en el index enlace el cdn de fontawesome para asi poder monstrar los iconos de las redes sociales, se centro el pid de pagina y se le agregaron los tiempos
    de animacion a todos los elementos del index

--- index.php
+++ index.php
@@ -11,67 +11,84 @@
   <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
   <!-- <script src="js/javascript.js"></script> -->
-	<title>Document</title>
+  <script src="https://use.fontawesome.com/57a0a1faad.js"></script>
+  <title>Document</title>
 </head>
 
 <!-- Star Body -->
 <body id="particleWrapper">
   <!-- particles.js container -->
-    <div id="particles-js"></div>
-<header class="header particle-title" >
+  <div id="particles-js"></div>
+  <header class="header particle-title" >
 
-  <nav class="navbar navbar-expand-lg">
-    <!-- Logo -->
-    <a class="navbar-logo" href="#">
-      <img class ="logo" src="img/zherodev_logo.png" alt="">
-    </a>
-    <!-- boton toggle cuando la vista es en movil -->
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <!-- lineas del boton -->
-      <span class="line"></span> 
-      <span class="line"></span> 
-      <span class="line" style="margin-bottom: 0;"></span>
+    <nav class=" animated fadeIn delay-5s navbar navbar-expand-lg">
+      <!-- Logo -->
+      <a class="navbar-logo" href="#">
+        <img class ="logo" src="img/zherodev_logo.png" alt="">
+      </a>
+      <!-- boton toggle cuando la vista es en movil -->
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <!-- lineas del boton -->
+        <span class="line"></span> 
+        <span class="line"></span> 
+        <span class="line" style="margin-bottom: 0;"></span>
 
-    </button>
-    <!-- Opciones de menu (contenedo) -->
-    <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
-      <!-- Opciones de menu lista -->
-      <ul class="navbar-nav ">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Active</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-        </li>
-      </ul>
-    </div>
-  </nav>  
-
-   <div class="container-fluid">
-    <div class="row">
+      </button>
+      <!-- Opciones de menu (contenedo) -->
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNavDropdown">
+        <!-- Opciones de menu lista -->
+        <ul class="navbar-nav ">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">Active</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Link</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+          </li>
+        </ul>
+      </div>
+    </nav>  
+    <!-- Nombre  -->
+    <div class="container-fluid">
+      <div class="row">
         <div class="col-lg-6">
           <h1 class="nombre animated fadeIn delay-5s" >EIBORTH GÓMEZ</h1>
-          <h5 class="maquina_escribir " id="texto" ></h5>       
+          <h5 class="maquina_escribir animated fadeIn delay-0.9s" id="texto" ></h5>       
         </div>
-        <div class="animated fadeIn delay-1s col-lg-6">
-           <div class="proyectos_intro"  alt="intro"></div>   
-        </div>
+        <div class="animated fadeIn delay-5s col-lg-6">
+         <div class="proyectos_intro"  alt="intro"></div>   
+       </div>
+     </div>
+   </div>
+   <!-- Pie de pagina -->
+   <footer style="top: 10%" class="main-footer animated fadeIn delay-5s">
+    <div class="container ">
+      <div class="row pie_pagina">
+        <div class="col-sm-12">
+          <div class="copyright">
+            Copyright © 2021 <a  href="https://zherodev.com">zherodev | by Eiborth Gómez</a>
+          </div>
+          <ul class="copyright social-links" >    
+            <li ><a href="" target="_blank"><i class="fa fa-github"></i></a></li>   
+            <li ><a href="" target="_blank"><i class="fa fa-linkedin"></i></a></li> 
+            <li ><a href="" target="_blank"><i class="fa fa-google-plus"></i></a></li>
+          </ul>
+        </div>  
+      </div>
     </div>
-</div>
-
+  </footer>
 </header>
 
 <!-- agrega un wrapper para asegurarse de que el título no se salga del área de partículas -->
 
-  <script src="js/javascript.js"></script>
-  <script src="js/particles.min.js"></script>
-  <script src="js/particulas.js"></script>
+<script src="js/javascript.js"></script>
+<script src="js/particles.min.js"></script>
+<script src="js/particulas.js"></script>
 </body>
 <!-- End Body -->
 
